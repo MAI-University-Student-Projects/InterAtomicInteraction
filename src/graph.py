52efbaa82a623d27195356c0557fa3f49b022e06
andrewhatake <andrewhatake@yandex.ru>
2021-04-03 01:43:44 +0300

    Upload Readme and final run

--- src/graph.py
+++ src/graph.py
@@ -23,7 +23,7 @@ for ax_i, lbl, u, u_min in zip(ax, lbl_l, ptncl_l, ptncl_min_list):
     ax_i.set_xlim(u_min[0] - 1, u_min[0] + 5)
     ax_i.set_ylim(u_min[1] - 1, u_min[1] + 5)
     ax_i.set_xlabel("r[A]")
-    ax_i.set_ylabel("u(a) [eV]")
+    ax_i.set_ylabel("u(r) [eV]")
     ax_i.grid(True)
     ax_i.plot(r, u)
 
